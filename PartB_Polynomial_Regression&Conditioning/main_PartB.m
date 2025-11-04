02edc1838382c1fadcd02f8ab6c9928579186ed2
Weiyao Yang <bruceyang11111@gmail.com>
2025-11-04 09:46:43 +0000

    add Part B(Polynomial Regression & Conditioning)

---
+++ PartB_Polynomial_Regression&Conditioning/main_PartB.m
@@ -0,0 +1,32 @@
+clc; clear; close all;
+
+T = 2*pi; dt = 0.001;
+omega = 2.0; phi = pi/6; sigma = 0.01;
+condV = zeros(1,10);
+err = zeros(1,10);
+
+[t, y, y_true] = generate_signal(dt, T, omega, phi, sigma);
+t_scaled = (t - mean(t)) / std(t);
+
+for n = 1:10
+    p = polyfit(t_scaled,y,n);
+    y_fit = polyval(p,t_scaled);
+    V = vander(t_scaled);
+    V = V(:, end-n:end);
+    condV(n) = cond(V' * V);
+    p_deriv = polyder(p);
+    y_deriv = polyval(p_deriv, t_scaled);
+    err(n) = sqrt(mean((y_deriv - y_true).^2));
+end
+
+figure;
+semilogy(1:10, condV, '-o')
+xlabel('Polynomial degree')
+ylabel('Condition number cond(V''V)')
+title('Condition vs polynomial degree')
+
+figure;
+semilogy(1:10, err, '-o');
+xlabel('Polynomial degree');
+ylabel('L_2 error of derivative');
+title('Derivative L_2 Error vs Polynomial Degree');
